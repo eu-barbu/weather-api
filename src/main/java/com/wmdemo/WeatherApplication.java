6d3d0fcbcfa77a9fc352903a652115a17b24898f
eu-barbu <eu.barbu@gmail.com>
2023-12-13 09:15:53 +0200

    Implementation of Weather API

--- src/main/java/com/wmdemo/WeatherApplication.java
+++ src/main/java/com/wmdemo/WeatherApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WiremockDemoApplication {
+public class WeatherApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WiremockDemoApplication.class, args);
+        SpringApplication.run(WeatherApplication.class, args);
     }
 }
