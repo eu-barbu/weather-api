e138d0d476385dbe0d0909c5a0373f18d865840e
eu-barbu <eu.barbu@gmail.com>
2023-12-15 09:53:40 +0200

    Implementation of Weather API

--- src/main/java/com/wmdemo/controller/WeatherController.java
+++ src/main/java/com/wmdemo/controller/WeatherController.java
@@ -14,24 +14,24 @@ public class WeatherController {
     @PostMapping(value = "/weather", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String weatherForecast(@ModelAttribute("location") String location) {
-
-        if (location == null || location.equals("")){
+        if (location == null || location.equals("")) {
             return "unkonwn location";
         }
 
         String content = Constants.WEATHER_TEMPLATE;
-            content = content.replace("${LOCATION}", location);
-            content = content.replace("${TEMPMIN}", String.valueOf(random(18.0, 25.0)));
-            content = content.replace("${TEMP}", String.valueOf(random(18.0, 25.0)));
-            content = content.replace("${TEMPFEEL}", String.valueOf(random(18.0, 25.0)));
-            content = content.replace("${TEMPMAX}", String.valueOf(random(18.0, 25.0)));
+        content = content.replace("${LOCATION}", location);
+        content = content.replace("${TEMPMIN}", String.valueOf(random()));
+        content = content.replace("${TEMP}", String.valueOf(random()));
+        content = content.replace("${TEMPFEEL}", String.valueOf(random()));
+        content = content.replace("${TEMPMAX}", String.valueOf(random()));
         return content;
     }
 
-
-    private double random(double min, double max) {
+    private double random() {
         Random r = new Random();
-        return (r.nextInt((int)((max-min)*10+1))+min*10) / 10.0;
+        double min = 18.0;
+        double max = 30.0;
+        return (r.nextInt((int) ((max - min) * 10 + 1)) + min * 10) / 10.0;
     }
 
 }
